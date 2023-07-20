67ef213edc298908a58c00e9403f587a215d1fa5
anand <anandsagar0006@gmail.com>
2023-07-20 22:36:34 +0530

    first commit

---
+++ app/src/main/java/com/example/myq/QuestionAnswer.java
@@ -0,0 +1,26 @@
+package com.example.myq;
+
+public class QuestionAnswer {
+
+    public static String question[] ={
+            "Which company owns the android?",
+            "Android is -?",
+            "Android is based on which of the following language?",
+            "APK stands for -?"
+    };
+
+    public static String choices[][] = {
+            {"Google","Apple","Nokia","Samsung"},
+            {"An operating system","a web browser","a web server","None of the above"},
+            {"Java","C++","C","Python"},
+            {"Android Phone Kit","Android Page Kit","Android Package Kit","None of the above"}
+    };
+
+    public static String correctAnswers[] = {
+            "Google",
+            "An operating system",
+            "Java",
+            "Android Package Kit"
+    };
+
+}
---
+++ app/src/main/java/com/example/myq/finish.java
@@ -0,0 +1,22 @@
+package com.example.myq;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class finish extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_finish2);
+    }
+
+    public void onClick(View view) {
+        Intent intent=new Intent(finish.this,MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
